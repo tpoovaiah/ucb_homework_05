63b204bc89bf4bdc6681252eb11d10db6bfc9944
tanyap <biddanda.tanya@gmail.com>
2020-05-09 10:15:32 -0700

    adds new theory about using this after tutor help

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 
+
 $(document).ready(function() {
     
+    $(".save").on("click", function(){
+        var value = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(value, time)
+    })
 
     function displayTime(){
-
 
         $(".time-block").each(function(){
         
             var time = moment()
             var currentTime = parseInt(time.hour())
             var timeBlock = parseInt($("#hour").attr("value"))
+
             //$("#current-time").text(currentTime)
-            //$("#current-day").text(currentTime.format('dddd'))
+            $("#current-day").text(time.format('dddd' + ", " + 'MMMM D' + " " + 'h:m:s A'))
 
             //var hour = currentTime.hour()
         
@@ -36,12 +42,16 @@ $(document).ready(function() {
             }
 
             //think about this below...apply "each" to each id? maybe .hour?
-            // 
-            //     var current = 
         });
     }
     
     displayTime();
+
+
+
+
+$("hour8 .description").val(localStorage.getItem("hour8"))
+
 
 
 
